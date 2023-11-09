592ae1b60ee370b1e025592b9e54daa6c6013f2e
devhoya97 <diajbh123@gmail.com>
2023-11-09 20:02:48 +0900

    feat(Order): 주문마다 메뉴와 개수를 가지며, 1개 이상의 주문만을 허용한다

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,4 +1,22 @@
 package christmas.domain;
 
+
+import christmas.domain.menu.Menu;
+
 public class Order {
+    private static void int MIN_NUMBER = 1;
+    private Menu menu;
+    private int number;
+
+    public Order(Menu menu, int number) {
+        validateNumber(number);
+        this.menu = menu;
+        this.number = number;
+    }
+
+    private void validateNumber(int number) {
+        if (number < MIN_NUMBER) {
+            throw new IllegalArgumentException(String.format("[ERROR] 최소 주문 개수는 %d개 입니다.", MIN_NUMBER));
+        }
+    }
 }
