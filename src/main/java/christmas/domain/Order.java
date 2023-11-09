9feeeadf9aa2a7ad3ff0bbdeafdc64e6bfd3ad48
devhoya97 <diajbh123@gmail.com>
2023-11-09 20:04:34 +0900

    feat(Order): 주문 하나에 대한 금액을 계산한다

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -4,9 +4,9 @@ package christmas.domain;
 import christmas.domain.menu.Menu;
 
 public class Order {
-    private static void int MIN_NUMBER = 1;
-    private Menu menu;
-    private int number;
+    private static final int MIN_NUMBER = 1;
+    private final Menu menu;
+    private final int number;
 
     public Order(Menu menu, int number) {
         validateNumber(number);
@@ -18,5 +18,9 @@ public class Order {
         if (number < MIN_NUMBER) {
             throw new IllegalArgumentException(String.format("[ERROR] 최소 주문 개수는 %d개 입니다.", MIN_NUMBER));
         }
+    }
+
+    public long calculatePrice() {
+        return menu.getPrice() * number;
     }
 }
