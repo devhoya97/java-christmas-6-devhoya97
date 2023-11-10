f66c60563ea658e9e499d7edc2c5dfc9cd5d7a4b
devhoya97 <diajbh123@gmail.com>
2023-11-10 14:59:38 +0900

    feat(VisitDay): 방문 날짜가 1~31사이가 아닐 경우 예외를 발생시키는 기능 구현

---
+++ src/main/java/christmas/domain/VisitDay.java
@@ -0,0 +1,24 @@
+package christmas.domain;
+
+import static christmas.domain.utils.ErrorMessage.DAY_OUT_OF_RANGE_ERROR;
+
+public class VisitDay {
+    public static final int MIN_DAY = 1;
+    public static final int MAX_DAY = 31;
+    private static final int CHRISTMAS = 25;
+    private static final int FRIDAY_REMAIN = 1;
+    private static final int SATURDAY_REMAIN = 2;
+    private static final int SUNDAY_REMAIN = 3;
+    private static final int WEEK_SIZE = 7;
+    private final int visitDay;
+    public VisitDay(int visitDay) {
+        validateDecember(visitDay);
+        this.visitDay = visitDay;
+    }
+
+    private void validateDecember(int visitDay) {
+        if (visitDay < MIN_DAY || visitDay > MAX_DAY) {
+            throw new IllegalArgumentException(String.format(DAY_OUT_OF_RANGE_ERROR, MIN_DAY, MAX_DAY));
+        }
+    }
+}
--- src/main/java/christmas/domain/utils/ErrorMessage.java
+++ src/main/java/christmas/domain/utils/ErrorMessage.java
@@ -4,4 +4,5 @@ public class ErrorMessage {
     public static final String PREFIX = "[ERROR] ";
     public static final String MAX_ORDER_ITEMS_ERROR = PREFIX + "주문한 메뉴당 개수의 총합은 최대 %d개까지 가능합니다.";
     public static final String MIN_ORDER_ITEM_ERROR = PREFIX + "선택한 메뉴의 개수가 %d개보다 작습니다.";
+    public static final String DAY_OUT_OF_RANGE_ERROR = PREFIX + "방문 날짜는 %d일~%d일 사이만 가능합니다.";
 }
