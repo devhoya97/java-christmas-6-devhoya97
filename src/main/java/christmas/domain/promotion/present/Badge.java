e53acd0133af4e6832d26fe07490229a8f1ad088
devhoya97 <diajbh123@gmail.com>
2023-11-10 17:29:55 +0900

    feat(BadgePromotion): 주문 금액에 맞게 이벤트 배지를 부여 기능 구현

---
+++ src/main/java/christmas/domain/promotion/present/Badge.java
@@ -0,0 +1,23 @@
+package christmas.domain.promotion.present;
+
+public enum Badge {
+    STAR("별", 5000),
+    TREE("트리", 10000),
+    SANTA("산타", 20000);
+
+    private final String name;
+    private final long acquisitionPrice;
+
+    Badge(String name, long acquisitionPrice) {
+        this.name = name;
+        this.acquisitionPrice = acquisitionPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getAcquisitionPrice() {
+        return acquisitionPrice;
+    }
+}
---
+++ src/main/java/christmas/domain/promotion/present/BadgePromotion.java
@@ -0,0 +1,22 @@
+package christmas.domain.promotion.present;
+
+public class BadgePromotion {
+    private final long totalDiscount;
+
+    public BadgePromotion(long totalDiscount) {
+        this.totalDiscount = totalDiscount;
+    }
+
+    public Badge getBadge() {
+        if (totalDiscount >= Badge.SANTA.getAcquisitionPrice()) {
+            return Badge.SANTA;
+        }
+        if (totalDiscount >= Badge.TREE.getAcquisitionPrice()) {
+            return Badge.TREE;
+        }
+        if (totalDiscount >= Badge.STAR.getAcquisitionPrice()) {
+            return Badge.STAR;
+        }
+        return null;
+    }
+}
