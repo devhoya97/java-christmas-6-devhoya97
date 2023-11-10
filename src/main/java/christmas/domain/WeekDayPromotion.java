45107caf81a915c8b6de2fc1cb893d1dca87ee87
devhoya97 <diajbh123@gmail.com>
2023-11-10 16:38:50 +0900

    feat(WeekDayPromotion): 평일 할인 적용시 할인금액 계산 기능 구현

--- src/main/java/christmas/domain/WeekDayPromotion.java
+++ src/main/java/christmas/domain/WeekDayPromotion.java
@@ -1,2 +1,23 @@
-package christmas.domain;public class WeekDayPromotion {
+package christmas.domain;
+
+public class WeekDayPromotion {
+    private static final long MIN_TOTAL_PRICE = 10000;
+    private static final long DISCOUNT_PER_DESSERT = 2023;
+    private static final long NO_DISCOUNT = 0;
+
+    private final VisitDay visitDay;
+    private final Order order;
+
+    public WeekDayPromotion(VisitDay visitDay, Order order) {
+        this.visitDay = visitDay;
+        this.order = order;
+    }
+
+    public long calculateDiscount() {
+        if ((visitDay.isInWeekDayPromotion()) && (order.calculateTotalPrice() >= MIN_TOTAL_PRICE)) {
+            int dessertCount = order.countDesserts();
+            return dessertCount * DISCOUNT_PER_DESSERT;
+        }
+        return NO_DISCOUNT;
+    }
 }
