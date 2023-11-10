c2bc771cdb74ad0cb7955d1ad4f213ea3ec176cb
devhoya97 <diajbh123@gmail.com>
2023-11-10 17:37:05 +0900

    test(BadgePromotionTest): 혜택 금액에 따라 배지 부여 기능 테스트

--- src/test/java/christmas/domain/promotion/present/BadgePromotionTest.java
+++ src/test/java/christmas/domain/promotion/present/BadgePromotionTest.java
@@ -1,4 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
+package christmas.domain.promotion.present;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.domain.VisitDay;
+import christmas.domain.menu.Drink;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 class BadgePromotionTest {
-  
+
+    @DisplayName("혜택 금액에 따라 배지를 부여한다.")
+    @ParameterizedTest
+    @CsvSource({"5000,별", "10000,트리", "20000,산타"})
+    void getBadge(int totalDiscount, String expectedResult) {
+        // given
+        BadgePromotion badgePromotion = new BadgePromotion(totalDiscount);
+        // when, then
+        assertThat(badgePromotion.getBadge().getName()).isEqualTo(expectedResult);
+    }
 }
