0150caf66926cfcbe29e14c56e31a98a894671e6
devhoya97 <diajbh123@gmail.com>
2023-11-10 17:03:17 +0900

    feat(SpecialPromotion): 특별 할인 적용 기능 구현

---
+++ src/main/java/christmas/domain/promotion/SpecialPromotion.java
@@ -0,0 +1,25 @@
+package christmas.domain.promotion;
+
+import christmas.domain.Order;
+import christmas.domain.VisitDay;
+
+public class SpecialPromotion {
+    private static final long MIN_TOTAL_PRICE = 10000;
+    private static final long DISCOUNT = 1000;
+    private static final long NO_DISCOUNT = 0;
+
+    private final VisitDay visitDay;
+    private final long totalPrice;
+
+    public SpecialPromotion(VisitDay visitDay, long totalPrice) {
+        this.visitDay = visitDay;
+        this.totalPrice = totalPrice;
+    }
+
+    public long calculateDiscount() {
+        if ((visitDay.isInSpecialPromotion()) && (totalPrice >= MIN_TOTAL_PRICE)) {
+            return DISCOUNT;
+        }
+        return NO_DISCOUNT;
+    }
+}
