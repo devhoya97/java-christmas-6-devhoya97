596a0fcb0fc54756ece4694a35039d3b595230ad
devhoya97 <diajbh123@gmail.com>
2023-11-10 16:32:07 +0900

    test(OrderTest): 주문 중 디저트 메뉴와 메인 메뉴의 총 개수를 계산 기능 테스트

--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -43,4 +43,34 @@ class OrderTest {
         // then
         assertThat(totalPrice).isEqualTo(expectedResult);
     }
+
+    @DisplayName("주문 중 디저트 메뉴의 총 개수를 계산한다.")
+    @Test
+    void countDesserts() {
+        // given
+        OrderItem tapas = new OrderItem(Appetizer.TAPAS, 1);
+        OrderItem christmasPasta = new OrderItem(Main.CHRISTMAS_PASTA, 2);
+        OrderItem chocoCake = new OrderItem(Dessert.CHOCO_CAKE, 3);
+        OrderItem iceCream = new OrderItem(Dessert.ICE_CREAM, 1);
+        OrderItem zeroCoke = new OrderItem(Drink.ZERO_COKE, 4);
+        Order order = new Order(List.of(tapas, christmasPasta, chocoCake, iceCream, zeroCoke));
+        long expectedResult = 4;
+        // when, then
+        assertThat(order.countDesserts()).isEqualTo(expectedResult);
+    }
+
+    @DisplayName("주문 중 메인 메뉴의 총 개수를 계산한다.")
+    @Test
+    void countMains() {
+        // given
+        OrderItem tapas = new OrderItem(Appetizer.TAPAS, 1);
+        OrderItem christmasPasta = new OrderItem(Main.CHRISTMAS_PASTA, 2);
+        OrderItem TBoneSteak = new OrderItem(Main.T_BONE_STEAK, 3);
+        OrderItem iceCream = new OrderItem(Dessert.ICE_CREAM, 1);
+        OrderItem zeroCoke = new OrderItem(Drink.ZERO_COKE, 2);
+        Order order = new Order(List.of(tapas, christmasPasta, TBoneSteak, iceCream, zeroCoke));
+        long expectedResult = 5;
+        // when, then
+        assertThat(order.countMains()).isEqualTo(expectedResult);
+    }
 }
