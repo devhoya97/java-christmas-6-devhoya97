91bdcde7727b96429e520f929325dd01b51f2963
devhoya97 <diajbh123@gmail.com>
2023-11-10 15:15:37 +0900

    test(VisitDayTest): 크리스마스 이벤트 기간을 확인하는 기능 테스트

--- src/test/java/christmas/domain/VisitDayTest.java
+++ src/test/java/christmas/domain/VisitDayTest.java
@@ -3,10 +3,12 @@ package christmas.domain;
 import static christmas.domain.utils.ErrorMessage.DAY_OUT_OF_RANGE_ERROR;
 import static christmas.domain.utils.ErrorMessage.MAX_ORDER_ITEMS_ERROR;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class VisitDayTest {
@@ -18,5 +20,17 @@ class VisitDayTest {
         assertThatThrownBy(() -> new VisitDay(visitDay))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(String.format(DAY_OUT_OF_RANGE_ERROR, VisitDay.MIN_DAY, VisitDay.MAX_DAY));
+    }
+
+    @DisplayName("방문 날짜가 크리스마스 이벤트 기간이 맞는지 알려준다.")
+    @ParameterizedTest
+    @CsvSource({"1,true", "25,true", "26,false"})
+    void creatInvalidVisitDay(int day, boolean expectedResult) {
+        // given
+        VisitDay visitDay = new VisitDay(day);
+        // when
+        boolean isInChristmasPromotion = visitDay.isInChristmasPromotion();
+        // then
+        assertThat(isInChristmasPromotion).isEqualTo(expectedResult);
     }
 }
