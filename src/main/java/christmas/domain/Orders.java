09a50e3ae60ec5b0420833b5e0e2ab92f0b7467b
devhoya97 <diajbh123@gmail.com>
2023-11-10 10:23:57 +0900

    feat(Orders): 주문 개수의 총 합이 20개가 넘을 경우 예외를 발생시킨다

--- src/main/java/christmas/domain/Orders.java
+++ src/main/java/christmas/domain/Orders.java
@@ -1,4 +1,23 @@
 package christmas.domain;
 
+import java.util.List;
+
 public class Orders {
+    private static final int MAX_TOTAL_ORDERS_NUMBER = 20;
+    List<Order> orders;
+
+    public Orders(List<Order> orders) {
+        validateTotalOrderNumber(orders);
+        this.orders = orders;
+    }
+
+    private void validateTotalOrderNumber(List<Order> orders) {
+        int totalOrderNumber = orders.stream()
+                .mapToInt(Order::getNumber)
+                .sum();
+        if (totalOrderNumber > MAX_TOTAL_ORDERS_NUMBER) {
+            throw new IllegalArgumentException(
+                    String.format("[ERROR] 주문 개수 총합은 최대 %d개 입니다.", MAX_TOTAL_ORDERS_NUMBER));
+        }
+    }
 }
