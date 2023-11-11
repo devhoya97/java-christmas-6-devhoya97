7b923d49e47bb39a5cf19531654339abbdfdd11c
devhoya97 <diajbh123@gmail.com>
2023-11-11 19:19:36 +0900

    refactor(OutputView): 가격에 쉼표처리 메서드 분리 및 변수명 변경

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -21,31 +21,42 @@ public class OutputView {
     private final static long NO_DISCOUNT = 0L;
     public static void printResult(VisitDate visitDate, Order order, BenefitManager benefitManager, Badge badge) {
         Map<Benefit, Long> benefitResult = benefitManager.getBenefitResult();
-        StringJoiner result = new StringJoiner(NEW_LINE);
-        result.add(String.format("12월 %s일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!", visitDate.toString()));
-        result.add("\n<주문 메뉴>").add(order.toString());
-        result.add("\n<할인 전 총주문 금액>").add(order.calculateTotalPrice() + "원");
-        addGiftMenuMessage(result, benefitResult);
-        addEachBenefitMessage(result, benefitResult);
-        addTotalBenefitMessage(result, benefitManager);
-        addTotalPriceAfterDiscountMessage(result, benefitManager);
-        result.add("\n<12월 이벤트 배지>").add(badge.getName());
-        System.out.println(result);
+        StringJoiner totalMessage = new StringJoiner(NEW_LINE);
+        totalMessage.add(String.format("12월 %s일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!", visitDate.toString()));
+        totalMessage.add("\n<주문 메뉴>").add(order.toString());
+        addTotalPriceMessage(totalMessage, order.calculateTotalPrice());
+        addGiftMenuMessage(totalMessage, benefitResult);
+        addEachBenefitMessage(totalMessage, benefitResult);
+        addTotalBenefitMessage(totalMessage, benefitManager);
+        addTotalPriceAfterDiscountMessage(totalMessage, benefitManager);
+        totalMessage.add("\n<12월 이벤트 배지>").add(badge.getName());
+        System.out.println(totalMessage);
     }
 
-    private static void addGiftMenuMessage(StringJoiner result, Map<Benefit, Long> benefitResult) {
-        result.add("\n<증정 메뉴>");
+    private static void addTotalPriceMessage(StringJoiner totalMessage, long totalPrice) {
+        totalMessage.add("\n<할인 전 총주문 금액>");
+        String thousandFormatTotalPrice = getThousandFormatPrice(totalPrice);
+        totalMessage.add(thousandFormatTotalPrice + MONEY_UNIT);
+    }
+
+    private static String getThousandFormatPrice(long price) {
+        DecimalFormat thousandSeparator = new DecimalFormat(DECIMAL_FORMAT);
+        return thousandSeparator.format(price);
+    }
+
+    private static void addGiftMenuMessage(StringJoiner totalMessage, Map<Benefit, Long> benefitResult) {
+        totalMessage.add("\n<증정 메뉴>");
         if (benefitResult.containsKey(Benefit.GIFT)) {
-            result.add(Menu.CHAMPAGNE.getName() + " 1개");
+            totalMessage.add(Menu.CHAMPAGNE.getName() + " 1개");
             return;
         }
-        result.add(NOTHING);
+        totalMessage.add(NOTHING);
     }
 
-    private static void addEachBenefitMessage(StringJoiner result, Map<Benefit, Long> benefitResult) {
-        result.add("\n<혜택 내역>");
+    private static void addEachBenefitMessage(StringJoiner totalMessage, Map<Benefit, Long> benefitResult) {
+        totalMessage.add("\n<혜택 내역>");
         if (benefitResult.isEmpty()) {
-            result.add(NOTHING);
+            totalMessage.add(NOTHING);
             return;
         }
         for (Entry<Benefit, Long> eachBenefit : benefitResult.entrySet()) {
@@ -53,27 +64,26 @@ public class OutputView {
             DecimalFormat thousandSeparator = new DecimalFormat(DECIMAL_FORMAT);
             String formattedBenefit = thousandSeparator.format(eachBenefit.getValue());
 
-            result.add(benefitName + BENEFIT_DELIMITER + MINUS + formattedBenefit + MONEY_UNIT);
+            totalMessage.add(benefitName + BENEFIT_DELIMITER + MINUS + formattedBenefit + MONEY_UNIT);
         }
     }
 
-    private static void addTotalBenefitMessage(StringJoiner result, BenefitManager benefitManager) {
-        result.add("\n<총혜택 금액>");
+    private static void addTotalBenefitMessage(StringJoiner totalMessage, BenefitManager benefitManager) {
+        totalMessage.add("\n<총혜택 금액>");
         long totalBenefit = benefitManager.calculateTotalBenefit();
         if (totalBenefit == NO_DISCOUNT) {
-            result.add(NO_DISCOUNT + MONEY_UNIT);
+            totalMessage.add(NO_DISCOUNT + MONEY_UNIT);
             return;
         }
         DecimalFormat thousandSeparator = new DecimalFormat(DECIMAL_FORMAT);
         String formattedBenefit = thousandSeparator.format(totalBenefit);
-        result.add(MINUS + formattedBenefit + MONEY_UNIT);
+        totalMessage.add(MINUS + formattedBenefit + MONEY_UNIT);
     }
 
-    private static void addTotalPriceAfterDiscountMessage(StringJoiner result, BenefitManager benefitManager) {
-        result.add("\n<할인 후 예상 결제 금액>");
+    private static void addTotalPriceAfterDiscountMessage(StringJoiner totalMessage, BenefitManager benefitManager) {
+        totalMessage.add("\n<할인 후 예상 결제 금액>");
         long totalPriceAfterDiscount = benefitManager.calculateTotalPriceAfterDiscount();
-        DecimalFormat thousandSeparator = new DecimalFormat(DECIMAL_FORMAT);
-        String formattedTotalPriceAfterDiscount = thousandSeparator.format(totalPriceAfterDiscount);
-        result.add(formattedTotalPriceAfterDiscount + MONEY_UNIT);
+        String thousandFormatTotalPriceAfterDiscount = getThousandFormatPrice(totalPriceAfterDiscount);
+        totalMessage.add(thousandFormatTotalPriceAfterDiscount + MONEY_UNIT);
     }
 }
