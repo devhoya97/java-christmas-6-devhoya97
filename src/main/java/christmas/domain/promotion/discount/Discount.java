8e78ee01ef1f0735f7242fea05f84af2328062b6
devhoya97 <diajbh123@gmail.com>
2023-11-11 12:48:08 +0900

    fix(Discount): WEEKEND의 이름이 평일 할인으로 잘못 기입되어 있어서 주말 할인으로 올바르게 수정

--- src/main/java/christmas/domain/promotion/discount/Discount.java
+++ src/main/java/christmas/domain/promotion/discount/Discount.java
@@ -3,7 +3,7 @@ package christmas.domain.promotion.discount;
 public enum Discount {
     CHRISTMAS("크리스마스 디데이 할인"),
     WEEK_DAY("평일 할인"),
-    WEEKEND("평일 할인"),
+    WEEKEND("주말 할인"),
     SPECIAL("특별 할인"),
     GIFT("증정 이벤트");
 
